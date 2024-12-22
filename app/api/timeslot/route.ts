7b8dba79703cbcfd0e6eff8fed7525958bce8768
Ny-Yanty <nyyanty117@gmail.com>
2024-12-22 23:52:41 +0700

    update

--- app/api/timeslot/route.ts
+++ app/api/timeslot/route.ts
@@ -87,8 +87,8 @@ export async function GET(
 
       return acc;
     }, []);
-    res.setHeader('Cache-Control', 'no-store, max-age=0');
-    return NextResponse.json(formattedData);
+    req.headers['Cache-Control'] = 'no-cache, max-age=0';
+    return NextResponse.json(formattedData || []);
   } catch (error: any) {
     console.error('Error fetching time slots:', error.message);
     return NextResponse.json({ message: 'Internal server error', error: error.message });
